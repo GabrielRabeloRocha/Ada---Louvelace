e4749c05f39469fcc08ec53940cf8b76ca724f56
GabrielRabeloRocha <gabrielrabelorocha@gmail.com>
2026-03-17 08:33:22 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function mostrarCuriosidade() {
+
+    document.getElementById("curiosidade").innerHTML =
+    "Ada Lovelace imaginou que computadores poderiam criar música e arte muito antes disso existir.";
+    
+    }
